97c0dacba1d33a1e1c088d74fa95ccce1d5b596a
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-04-30 20:25:40 +0200

    Description concise des changements apportés

--- src/components/InstagramPost.jsx
+++ src/components/InstagramPost.jsx
@@ -28,7 +28,7 @@ function InstagramPost() {
                 <img src={Avatar} alt="Avatar" className='h-auto w-auto object-cover' />
                 <BsThreeDots />    
         </div>
-        <img src={post.media_url} alt={post.caption} className="w-full object-cover" />
+        <img src={post.media_url} alt={post.caption} className="w-full" />
         <div className="flex space-x-4 py-2">
           <FaHeart className="text-red-500" />
           <FaComment />
