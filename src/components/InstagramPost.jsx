faddd1b7e003422eab5ba40c1d26fe770d2c6262
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-04-30 21:04:13 +0200

    Description concise des changements apportés

--- src/components/InstagramPost.jsx
+++ src/components/InstagramPost.jsx
@@ -46,8 +46,8 @@ function InstagramPost() {
           <FaComment />
           <FaShareSquare />
         </div>
-        <p className="text-sm px-4 py-2 overflow-hidden text-ellipsis">{post.caption}</p>
-        <p className='text-gray-400 text-sm px-2'>View all 16 comments</p>
+        <p className="text-sm px-4 py-2 overflow-hidden text-ellipsis text-nowrap xl:text-wrap">{post.caption}</p>
+        <p className='text-gray-400 text-sm px-4'>View all 16 comments</p>
         <div className='flex justify-center items-center'>
         <a href={post.permalink} target="_blank" rel="noopener noreferrer" className="text-secondary text-center underline py-2">
           Rejoins-nous sur instagram!
