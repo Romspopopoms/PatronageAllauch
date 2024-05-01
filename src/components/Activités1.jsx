aac4e7692cceb6b5f387815f6c784de71a83ede5
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-05-01 11:24:09 +0200

    Description concise des changements apportés

--- src/components/Activités1.jsx
+++ src/components/Activités1.jsx
@@ -4,7 +4,7 @@ import ModalActivités from "./ModalActivités";
 
 const Activités1 = () => {
     return (
-        <div className="relative flex flex-col h-auto w-auto">
+        <div className="relative flex flex-col h-auto w-auto ">
             <img src={Banniere} alt="bannièreActivités" className="h-auto w-auto object-cover" />
             <ModalActivités />
         </div>
