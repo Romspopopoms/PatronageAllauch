9ddea6da712d2c43ce4ddebc295d790fd9339fa0
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-05-01 15:19:47 +0200

    Description concise des changements apportés

--- api/SendEmail.js
+++ api/SendEmail.js
@@ -1,29 +1,49 @@
 import nodemailer from 'nodemailer';
 
-// Créer un transporteur Nodemailer qui utilise l'authentification OAuth2
+// Configuration de transporter pour l'authentification OAuth2 avec Nodemailer
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     type: 'OAuth2',
-    user: 'masaintebible.fr@gmail.com', // L'adresse email que vous utilisez pour envoyer des e-mails
+    user: 'masaintebible.fr@gmail.com',
     clientId: process.env.GMAIL_CLIENT_ID,
     clientSecret: process.env.GMAIL_CLIENT_SECRET,
     refreshToken: process.env.GMAIL_REFRESH_TOKEN,
   }
 });
 
-// Fonction pour envoyer des e-mails
+// Fonction pour envoyer un email de confirmation à l'expéditeur du formulaire
+async function sendConfirmationEmail(formData) {
+  const { name, firstName, email } = formData;
+  const mailOptions = {
+    from: 'masaintebible.fr@gmail.com',
+    to: email,
+    subject: "Votre Message à bien été reçu",
+    text: `Bonjour ${name} ${firstName}, nous vous informons que nous avons bien reçu votre message.`
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    return 'Confirmation email sent successfully';
+  } catch (error) {
+    console.error('Error sending confirmation email:', error);
+    throw error;
+  }
+}
+
+// Fonction pour envoyer un email avec les informations du formulaire au propriétaire
 async function sendEmail(formData) {
   const { name, firstName, email, phone, message } = formData;
   const mailOptions = {
-    from: 'masaintebible.fr@gmail.com', // L'adresse Gmail utilisée pour OAuth2
-    to: 'masaintebible.fr@gmail.com', // Votre propre adresse où vous souhaitez recevoir les infos du formulaire
+    from: 'masaintebible.fr@gmail.com',
+    to: 'masaintebible.fr@gmail.com',
     subject: 'Nouveau message du formulaire de contact',
     text: `
-            Vous avez reçu un message de 
-            ${name} ${firstName} (${email}):
-            Téléphone : ${phone}
-            Message : ${message}`
+      Vous avez reçu un message de 
+      ${name} ${firstName} (${email}):
+      Téléphone : ${phone}
+      Message : ${message}
+    `
   };
 
   try {
@@ -40,7 +60,8 @@ async function emailHandler(req, res) {
   if (req.method === 'POST') {
     try {
       const result = await sendEmail(req.body);
-      res.status(200).send({ message: result });
+      const confirmationResult = await sendConfirmationEmail(req.body);
+      res.status(200).send({ message: result, confirmationMessage: confirmationResult });
     } catch (error) {
       res.status(500).send({ error: 'Failed to send email', details: error.message });
     }
